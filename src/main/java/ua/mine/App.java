aa6419b82c7b310944d0d5fecf642b2dcd0bc150
stepan <stepan_yershov@epam.com>
2019-01-29 15:41:59 +0200

    init commit

---
+++ src/main/java/ua/mine/App.java
@@ -0,0 +1,14 @@
+package ua.mine;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import quoters.Quoter;
+import quoters.TerminatorQuoter;
+
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("context.xml");
+
+            context.getBean(Quoter.class);
+    }
+}
